703b35ee2007a67ac8e2b197aa8cd96b1ae63ee9
Agnès <agnes.toulet@free.fr>
2015-03-31 16:24:54 +0200

    Bons fichiers

--- frameself_interface/src/SpecificCollector.java
+++ frameself_interface/src/SpecificCollector.java
@@ -1,33 +1,136 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
 import java.util.Date;
 
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
 import com.phidgets.InterfaceKitPhidget;
-import com.phidgets.PhidgetException;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
 import frameself.Collector;
 import frameself.format.Event;
+
+
 public class SpecificCollector {
 	//Use this InterfaceKitPhidget to monitor and control phidgets devices
 	public static InterfaceKitPhidget ik;
 	// Create a collector that publishes events to FRAMSELF on 127.0.0.1:5000
-	public static Collector collector = new Collector("127.0.0.1",5000);
+	public static Collector collector = new Collector("127.0.0.1", 5000);
 
+	private static int port = 6300;
+    private static String context = "/collector";
+	
 	public static void start() {
-		while(true){
-			// Example of event (To be replaced by the real Phidgets event)
-			Event event = new Event();
-			event.setCategory("Luminosity");
-			event.setValue("200");
-			event.setSensor("LuminositySensor");
-			event.setLocation("Home");
-			event.setTimestamp(new Date());
-			event.setExpiry(new Date(System.currentTimeMillis()+20000));
-			// Send the event to FRAMSELF
-			collector.send(event);
+
+		System.out.println("Starting server..");
+		HttpServer server;
+		try {
+			server = HttpServer.create(new InetSocketAddress(port), 0);
+			server.createContext(context, new MyHandler());
+			server.start();
+			System.out.println("The server is now listening on\nPort: "+ port+"\nContext: "+context+"\n");
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	static class MyHandler implements HttpHandler {
+		public void handle(HttpExchange t) throws IOException {
+
+			String body = "";
+			int i;
+			char c;
 			try {
-				// waiting for 1 second
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
+				InputStream is = t.getRequestBody();
+
+
+				while ((i = is.read()) != -1) {
+					c = (char) i;
+					body = (String) (body + c);
+				}
+			} catch (Exception e) {
 				e.printStackTrace();
+			}
+			System.out.println("Received notification:");
+			System.out.println(body);
+
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			try {
+				DocumentBuilder dBuilder = dbf.newDocumentBuilder();
+
+				Document notifyDoc = dBuilder.parse(new InputSource(new ByteArrayInputStream(body.getBytes("utf-8"))));
+
+				String contentInstance64 = notifyDoc.getElementsByTagName("om2m:representation").item(0).getTextContent();
+				System.out.println("ContentInstance (Base64-encoded):\n"+contentInstance64+"\n");
+
+				String contentInstance = new String(DatatypeConverter.parseBase64Binary(contentInstance64));
+				System.out.println("ContentInstance:\n"+contentInstance+"\n");
+
+				Document instanceDoc = dBuilder.parse(new InputSource(new ByteArrayInputStream(contentInstance.getBytes("utf-8"))));
+				String content64 = instanceDoc.getElementsByTagName("om2m:content").item(0).getTextContent();;
+				System.out.println("Content (Base64-encoded):\n"+content64+"\n");
+
+				final String content = new String(DatatypeConverter.parseBase64Binary(content64));
+				System.out.println("Content:\n"+content+"\n");
+
+				t.sendResponseHeaders(204, -1);
+				
+				Document finalDoc = dBuilder.parse(new ByteArrayInputStream(content.getBytes("utf-8")));
+				finalDoc.getDocumentElement().normalize();
+				Event event = new Event();
+				
+				System.out.println("Root element :" + finalDoc.getDocumentElement().getNodeName());
+				NodeList nList = finalDoc.getDocumentElement().getChildNodes();
+				
+				for (int temp = 0; temp < nList.getLength(); temp++) {
+				 
+					Node node = nList.item(temp);			 
+					System.out.println("\nCurrent Element :" + node.getNodeName());
+			 
+					if (node.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) node;
+						switch(eElement.getAttribute("name")) {
+							case "type":  event.setCategory("Luminosity"); System.out.println("setCategory"); break;
+							case "location": event.setLocation(eElement.getAttribute("val")); System.out.println("setLcoation");break;
+							case "appId": event.setSensor("LuminositySensor"); System.out.println("setSensor");break;
+							case "data": event.setValue(eElement.getAttribute("val"));System.out.println("setValue");break;
+							default: break;
+						}					
+					}
+				}
+				
+				event.setTimestamp(new Date());
+				event.setExpiry(new Date(System.currentTimeMillis()+20000));
+				
+				// Send the event to FRAMSELF
+				collector.send(event);
+
+			} catch (ParserConfigurationException e) {
+				e.printStackTrace();
+				t.sendResponseHeaders(501, -1);
+			} catch (SAXException e) {
+				e.printStackTrace();
+				t.sendResponseHeaders(501, -1);
+			} catch (IOException e) {
+				e.printStackTrace();
+				t.sendResponseHeaders(501, -1);
 			}
 		}
 	}
--- frameself_interface/src/SpecificDispatcher.java
+++ frameself_interface/src/SpecificDispatcher.java
@@ -1,17 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import frameself.Dispatcher;
 import frameself.format.Action;
+
 public class SpecificDispatcher {
 	// Create a dispatcher that listens to actions from FRAMESELF on port 7000 and
 	// sends back results on 127.0.0.1:6000
 	public static Dispatcher dispatcher = new Dispatcher("127.0.0.1",6000,7000);
+	private static HttpURLConnection server;
 	
 	public static void start() {
 		
+	    try {
+	    	// Envoi de la requête pour s'inscrire à la ressource SENSOR_O
+			connect("http://127.0.0.1:8080/om2m/gscl/applications/SENSOR_0/containers/DATA/contentInstances/subscriptions", "POST");
+			post("<om2m:subscription xmlns:om2m=\"http://uri.etsi.org/m2m\">"
+					+ "<om2m:contact>http://127.0.0.1:6300/collector</om2m:contact>"
+					+ "</om2m:subscription>");
+			displayResponse();
+			disconnect();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
 		while(true){
 			// Receive an action from FRAMSELF
 			Action action = dispatcher.receive();
 			// TODO Execute action (To be replaced by a real Phidgets action) 
 			System.out.println(action.getName()+" excuted");
+//			switch(action.getName()) {
+//				case "SwitchLampOn":
+//					connect("http://127.0.0.1:8080/om2m/gscl/applications/OUTPUT_2/containers/DATA/contentInstances", "POST");
+//					post("<obj><str name=\"type\" val=\"OUTPUT\"/>"
+//							+ "<str name=\"location\" val=\"Home\"/>"
+//							+ "<str name=\"appId\" val=\"OUTPUT_2\"/>"
+//							+ "<int name=\"data\" val=\"1\"/>"
+//							+ "</obj>");
+//					displayResponse();
+//					disconnect();
+//					break;
+//			}
+//			
 			// Update the action result and error attributes
 			action.setResult("true");
 			action.setError("No error");
@@ -19,4 +54,67 @@ public class SpecificDispatcher {
 			dispatcher.send(action);
 		}
 	}
+	
+	 public static void connect(String urlS, String method)
+	  {
+	    try
+	    {
+	      URL url = new URL(urlS);
+	      server = (HttpURLConnection)url.openConnection();
+	      server.setDoInput(true);
+	      server.setDoOutput(true);
+	      server.setRequestMethod(method);
+          server.setRequestProperty("Authorization", "Basic YWRtaW46YWRtaW4=");
+	      server.connect();
+	    }
+	    catch (Exception e)
+	    {
+	      System.out.println("Connection failed");
+	    }
+	  }
+
+	  public static void disconnect()
+	  {
+	    server.disconnect();
+	  }
+
+	  public static void displayResponse()
+	  {
+	    String line;
+
+	    try
+	    {
+	      BufferedReader s = new BufferedReader(
+	                            new InputStreamReader(
+	                                server.getInputStream()));
+	      line = s.readLine();
+	      while (line != null)
+	      {
+	        System.out.println(line);
+	        line = s.readLine();
+	      }
+	      s.close();
+	    }
+	    catch(Exception e)
+	    {
+	      System.out.println("Unable to read input stream");
+	    }
+	  }
+
+	  public static void post(String s)
+	  {
+	    try
+	    {
+	      BufferedWriter bw = new BufferedWriter(
+	                                new OutputStreamWriter(
+	                                    server.getOutputStream()));
+	      bw.write(s, 0, s.length());
+	      bw.flush();
+	      bw.close();
+	    }
+	    catch(Exception e)
+	    {
+	      System.out.println("Unable to write to output stream");
+	    }
+	  }
 }
